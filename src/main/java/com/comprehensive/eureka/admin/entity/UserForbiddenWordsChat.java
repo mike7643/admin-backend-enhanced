212934dacc7845e6e272db1a318d5769feb4458c
mike7643 <developerJYumin@gmail.com>
2025-06-12 20:04:38 +0900

    feat: 사용자 금칙어 엔티티에 삭제 시 연쇄 삭제 설정 추가

--- src/main/java/com/comprehensive/eureka/admin/entity/UserForbiddenWordsChat.java
+++ src/main/java/com/comprehensive/eureka/admin/entity/UserForbiddenWordsChat.java
@@ -2,6 +2,8 @@ package com.comprehensive.eureka.admin.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "user_forbidden_words_chat",
@@ -26,5 +28,6 @@ public class UserForbiddenWordsChat {
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "forbidden_word_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private ForbiddenWord forbiddenWord;
 }
